bdddf332741f6488d8caa8ef5e6f5872a3981d31
kcksmart <60355329+kcksmart@users.noreply.github.com>
2020-12-11 05:44:38 +0530

    Add files via upload

---
+++ layers/Entwurfselement_Lage_16.js
@@ -0,0 +1,8 @@
+var json_Entwurfselement_Lage_16 = {
+"type": "FeatureCollection",
+"name": "Entwurfselement_Lage_16",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+
+]
+}
